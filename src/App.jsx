fa520293fe8bc85ae0a2cbdd9ea64cdfac3a2c6a
Pratik Kuril <pratikkuril16@gmail.com>
2025-01-28 21:49:29 +0530

    Add assets and create Hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
+import Search from './components/Search'
 
 const App = () => {
   return (
-    <div>
-      <h1 className='text-3xl font-bold'>
-        Hello World!
-      </h1>
-    </div>
+    <main>
+      <div className='pattern'/>
+      <div className="wrapper">
+        <header>
+          <img src="./hero.png" alt="Hero Banner" />
+          <h1>Find <span className='text-gradient'>Movies</span> You'll Enjoy Without The Hassle</h1>
+        </header>
+        <p>Search</p>
+        <Search />
+      </div>
+    </main>
   )
 }
 
