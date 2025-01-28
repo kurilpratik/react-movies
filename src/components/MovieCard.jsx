54ca2a4c057140a104be4fe27119ec8ba32e6430
Pratik Kuril <pratikkuril16@gmail.com>
2025-01-29 04:57:11 +0530

    Change to alive assets

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -14,7 +14,7 @@ const MovieCard = ({ movie:  // Destructuring movie itself
             <div className="content">
                 
                 <div className="rating">
-                    <img src="/star.svg" alt="star-icon" />
+                    <img src="./star.svg" alt="star-icon" />
                     <p>{vote_average ? vote_average.toFixed(1) : 'N/A'}</p>
                 </div>
 
--- vite.config.js
+++ vite.config.js
@@ -8,5 +8,5 @@ export default defineConfig({
     react(),
     tailwindcss()
   ],
-  base: "/react-movies"
+  base: "/"
 })
