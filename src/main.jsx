ce5cdcc2f2c7adadea34aa8689612189b3a3a1f0
Pratik Kuril <pratikkuril16@gmail.com>
2025-03-07 01:23:24 +0530

    Set up basic react router and add Movie page

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
 import App from './App'
+import Movie from './Movie'
+
+const router = createBrowserRouter([
+    // { path:"/react-movies", element: <App />},
+    { path:`${import.meta.env.BASE_URL}`, element: <App />},
+    { path:`${import.meta.env.BASE_URL}/movie`, element: <Movie />}
+])
 
 createRoot(document.getElementById('root')).render(
-    <App />
+    <RouterProvider router={router}/>
 )
